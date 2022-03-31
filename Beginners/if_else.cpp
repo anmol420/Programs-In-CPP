8e7059dfe02a2d14c402224b9ca6318093d0bbb5
Anmol Anand <anmolanand2k15@gmail.com>
2022-03-31 12:24:50 +0530

    Added Some Beginner Level Programs

---
+++ Beginners/if_else.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 12;
+    if (n<=11) {
+        cout<<"Number Less Than 11"<<endl;
+    } else {
+        cout<<"Number Greater Than 11"<<endl;
+    }
+    return 0;
+}
