5da382d77236f5534aff3cf2953d927d7559bf8c
Anmol Anand <anmolanand2k15@gmail.com>
2022-04-02 20:08:47 +0530

    Added Beginner Programs

---
+++ Beginners/division.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int div = 4/3;
+    int mod = 5%2;
+    cout<<"Division = "<<div<<endl;
+    cout<<"Modulus = "<<mod<<endl;
+    return 0;
+}
